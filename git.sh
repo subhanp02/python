3439d22daeec9d1de6f7759579616815c6c11176
subhan <subhan@123>
2024-11-05 11:59:16 +0530

    git.sh

--- git.sh
+++ git.sh
@@ -11,6 +11,7 @@
     git add index.html 
     git commit - initial commit 
     git commit -m "added more htmls"
+    git commit -a -m "added more htmls"
 
 # UNDO FILES DATA
     git checkout <files_name>
@@ -27,3 +28,9 @@
 # cashed command
     git rm <file_name>  - will be  delete file working directory 
     git rm --cashed <file_name> - untracked file 
+
+# BRANCH 
+# CREATE BRANCH
+    git branch - shows your current branch
+    git branch feature1 -  create branch
+    git checkout feature1 - swith to checkout feature1 
