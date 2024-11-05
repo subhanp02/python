332b6847af51ad83fe11e6d833d9bc1e48e2d191
subhan <subhan@123>
2024-11-05 11:08:20 +0530

    added more htmls

---
+++ git.sh
@@ -0,0 +1,12 @@
+# CREATE USERNAME AND EMAILS
+    git config --global user.name <your_user_name>
+
+    git config --global user.email <your_email_name>
+
+# OPEN VS CODE
+    code .
+
+# CHECK STATUS
+    git status 
+    git add index.html 
+    git commit 
